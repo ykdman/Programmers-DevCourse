74d759c3ae1851c1135d19d47757fadbeec751b4
YKDman <dbsrudejr300@gmail.com>
2024-04-25 13:30:19 +0900

    DONE 2024-04-25

---
+++ prac-express/app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const port = 3000;
+
+// express 객체 사용 선언
+const app = express();
+
+// root url response
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+/**app.get
+ * arg1 : url
+ * arg2 : callbackFn (request, response)
+ */
+app.get("/test", (req, res) => {
+  res.send("Test API");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("안녕하세여");
+});
+app.get("/bye", (req, res) => {
+  res.send("안녕히 가세요");
+});
+
+app.get("/meet", (req, res) => {
+  res.send("만나서 반갑습니다.");
+});
+
+app.get("/products/:item", (req, res) => {
+  const item = +req.params.item;
+  if (item > 5) {
+    console.log("5보다 큰 수 가 들어왔습니다.");
+  }
+  const testObj = {
+    name: "kdman",
+    age: 26,
+    loc: "seoul",
+    item,
+  };
+
+  console.log("GET Object");
+  console.dir(testObj);
+
+  console.log("Request Params");
+  console.dir(req.params);
+
+  res.send(testObj);
+});
+
+app.get("/watch", (req, res) => {
+  const querys = req.query;
+  console.log(querys);
+
+  res.send({ ...querys });
+});
+
+// port : 3000
+app.listen(port, () => {
+  console.log(`Server Listening : ${port}`);
+});
---
+++ prac-express/map-demo.js
@@ -0,0 +1,8 @@
+const db = new Map();
+
+db.set(1, "hi");
+db.set(2, "test");
+db.set(3, "dbTe");
+
+console.dir(db);
+console.log(db.get(1));
